eaa682d6f5acb6e4fff6813efcce69ec699b0231
유현명 <aahspringaa4@naver.com>
2022-01-15 16:49:55 +0900

    ⚡️:: Request값 구현

--- app/src/main/java/com/example/sdk_project/ui/auth/dto/RegisterRequestDTO.kt
+++ app/src/main/java/com/example/sdk_project/ui/auth/dto/RegisterRequestDTO.kt
@@ -1,4 +1,8 @@
 package com.example.sdk_project.ui.auth.dto
 
-class RegisterRequestDTO {
-}
+import com.google.gson.annotations.SerializedName
+
+data class RegisterRequestDTO(
+    @SerializedName("id") val id: Int,
+    @SerializedName("password") val password: String
+)
