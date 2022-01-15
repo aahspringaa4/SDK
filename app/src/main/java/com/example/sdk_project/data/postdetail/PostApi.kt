6d873a629e34aecc099e5703cc760e36e741f807
유현명 <aahspringaa4@naver.com>
2022-01-15 18:03:03 +0900

    ⚡️:: 게시물 자세히보기 network 구현

--- app/src/main/java/com/example/sdk_project/data/postdetail/PostApi.kt
+++ app/src/main/java/com/example/sdk_project/data/postdetail/PostApi.kt
@@ -1,4 +1,17 @@
 package com.example.sdk_project.data.postdetail
 
+import com.example.sdk_project.ui.main.dto.MainResponseDTO
+import com.example.sdk_project.ui.postdetail.dto.PostResponseDTO
+import io.reactivex.rxjava3.core.Single
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
 interface PostApi {
+    @GET("post")
+    fun readPostDetail(
+        @Query("id") id: Int,
+        @Header("Authorization") accessToken: String
+    ) : Single<Response<PostResponseDTO>>
 }
