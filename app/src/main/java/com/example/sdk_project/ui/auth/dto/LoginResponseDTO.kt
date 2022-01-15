d8fcbaf0d2ea9fee9c3719fe41d39fc229244dac
유현명 <aahspringaa4@naver.com>
2022-01-15 18:36:11 +0900

    ⚰️:: RefreshToken

--- app/src/main/java/com/example/sdk_project/ui/auth/dto/LoginResponseDTO.kt
+++ app/src/main/java/com/example/sdk_project/ui/auth/dto/LoginResponseDTO.kt
@@ -3,6 +3,5 @@ package com.example.sdk_project.ui.auth.dto
 import com.google.gson.annotations.SerializedName
 
 data class LoginResponseDTO(
-    @SerializedName("access_token") val accessToken: String,
-    @SerializedName("refresh_token") val refreshToken: String
+    @SerializedName("access_token") val accessToken: String
 )
