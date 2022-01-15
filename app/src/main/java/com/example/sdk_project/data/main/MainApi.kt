8b28f8e7e607b4bffc661bed49c16a0cfb131ffa
유현명 <aahspringaa4@naver.com>
2022-01-15 16:36:09 +0900

    ⚡️:: Main NetWork 구현

--- app/src/main/java/com/example/sdk_project/data/main/MainApi.kt
+++ app/src/main/java/com/example/sdk_project/data/main/MainApi.kt
@@ -1,4 +1,20 @@
 package com.example.sdk_project.data.main
 
+import com.example.sdk_project.ui.createpost.dto.CreatePostRequestDTO
+import com.example.sdk_project.ui.createpost.dto.CreatePostResponseDTO
+import com.example.sdk_project.ui.main.dto.MainRequestDTO
+import com.example.sdk_project.ui.main.dto.MainResponseDTO
+import io.reactivex.rxjava3.core.Single
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
 interface MainApi {
+    @GET("post")
+    fun readPostList(
+        @Query("id") id: Int,
+        @Body mainRequestDTO: MainRequestDTO
+    ) : Single<Response<MainResponseDTO>>
 }
