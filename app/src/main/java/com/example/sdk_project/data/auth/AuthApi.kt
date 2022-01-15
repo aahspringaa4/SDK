2c37ff3b66da1918829d74949da3fc1b6fd553fa
유현명 <aahspringaa4@naver.com>
2022-01-15 16:49:30 +0900

    ♻️:: Interface Error

--- app/src/main/java/com/example/sdk_project/data/auth/AuthApi.kt
+++ app/src/main/java/com/example/sdk_project/data/auth/AuthApi.kt
@@ -3,7 +3,6 @@ package com.example.sdk_project.data.auth
 import com.example.sdk_project.ui.auth.dto.LoginRequestDTO
 import com.example.sdk_project.ui.auth.dto.LoginResponseDTO
 import com.example.sdk_project.ui.auth.dto.RegisterRequestDTO
-import com.example.sdk_project.ui.auth.dto.RegisterResponseDTO
 import io.reactivex.rxjava3.core.Single
 import retrofit2.Response
 import retrofit2.http.Body
@@ -11,12 +10,10 @@ import retrofit2.http.POST
 
 interface AuthApi {
     @POST("user/signup")
-    fun postRegister(
-        @Body registerRequestDTO: RegisterRequestDTO
-    ) : Single<Response<RegisterResponseDTO>>
+    fun postRegister(@Body registerRequestDTO: RegisterRequestDTO)
 
     @POST("user/signin")
     fun postLogin(
         @Body loginRequestDTO: LoginRequestDTO
-    ) : Single<Response<LoginResponseDTO>>
+    ): Single<Response<LoginResponseDTO>>
 }
