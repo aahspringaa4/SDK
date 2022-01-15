58251fa5c3c8d96b358057cfd482e1262c668574
유현명 <aahspringaa4@naver.com>
2022-01-15 15:48:45 +0900

    ⚡️:: MypageViewModel 구현

---
+++ app/src/main/java/com/example/sdk_project/ui/mypage/viewmodel/MypageViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.sdk_project.ui.mypage.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.sdk_project.data.auth.LoginRepository
+import com.example.sdk_project.data.mypage.MypageRepository
+
+class MypageViewModel(
+    private val rp: MypageRepository
+) : ViewModel() {
+
+}
