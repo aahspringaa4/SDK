dfc5518e4ec15e173f0eb27ce90874f70520897b
유현명 <aahspringaa4@naver.com>
2022-01-15 21:59:34 +0900

    ⚡️:: MypageRepository 구현

--- app/src/main/java/com/example/sdk_project/data/mypage/MypageRepository.kt
+++ app/src/main/java/com/example/sdk_project/data/mypage/MypageRepository.kt
@@ -1,4 +1,17 @@
 package com.example.sdk_project.data.mypage
 
+import com.example.sdk_project.data.ACCESS_TOKEN
+import com.example.sdk_project.data.myPageApi
+import com.example.sdk_project.ui.mypage.dto.MypageResponseDTO
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import retrofit2.Response
+
 class MypageRepository {
+    fun readMyPage() : @NonNull Single<Response<MypageResponseDTO>> =
+        myPageApi.readMyPage(ACCESS_TOKEN)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
 }
