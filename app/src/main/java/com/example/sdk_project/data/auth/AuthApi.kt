200c8915135979c825e51926b7f45a3495dbb9fa
유현명 <aahspringaa4@naver.com>
2022-01-15 16:20:07 +0900

    📑:: Auth Api 구현

--- app/src/main/java/com/example/sdk_project/data/auth/AuthApi.kt
+++ app/src/main/java/com/example/sdk_project/data/auth/AuthApi.kt
@@ -1,4 +1,22 @@
 package com.example.sdk_project.data.auth
 
+import com.example.sdk_project.ui.auth.dto.LoginRequestDTO
+import com.example.sdk_project.ui.auth.dto.LoginResponseDTO
+import com.example.sdk_project.ui.auth.dto.RegisterRequestDTO
+import com.example.sdk_project.ui.auth.dto.RegisterResponseDTO
+import io.reactivex.rxjava3.core.Single
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface AuthApi {
+    @POST("user/signup")
+    fun postRegister(
+        @Body registerRequestDTO: RegisterRequestDTO
+    ) : Single<Response<RegisterResponseDTO>>
+
+    @POST("user/signin")
+    fun postLogin(
+        @Body loginRequestDTO: LoginRequestDTO
+    ) : Single<Response<LoginResponseDTO>>
 }
