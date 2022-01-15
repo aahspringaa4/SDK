29e71b0a59bbe5e210ceecca0723892f8b076e91
유현명 <aahspringaa4@naver.com>
2022-01-15 18:10:01 +0900

    ⚡️:: MainResponseDTO 구현

--- app/src/main/java/com/example/sdk_project/ui/main/dto/MainResponseDTO.kt
+++ app/src/main/java/com/example/sdk_project/ui/main/dto/MainResponseDTO.kt
@@ -1,4 +1,14 @@
 package com.example.sdk_project.ui.main.dto
 
-class MainResponseDTO {
+import com.google.gson.annotations.SerializedName
+
+data class MainResponseDTO(
+    @SerializedName("post") val post: PostList
+){
+    data class PostList(
+        @SerializedName("title") val postTitle: String,
+        @SerializedName("url") val postUrl: String,
+        @SerializedName("category") val postCategory: String,
+        @SerializedName("user") val writer:String
+    )
 }
