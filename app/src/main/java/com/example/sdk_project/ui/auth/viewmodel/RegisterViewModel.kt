6d9df1a3c68bf649ae34312eff38fbaf8a0c43fa
유현명 <aahspringaa4@naver.com>
2022-01-15 15:48:27 +0900

    ⚡️:: RegisterViewModel 상속 구현

---
+++ app/src/main/java/com/example/sdk_project/ui/auth/viewmodel/RegisterViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.sdk_project.ui.auth.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.sdk_project.data.auth.RegisterRepository
+
+class RegisterViewModel(
+    private val rp: RegisterRepository
+) : ViewModel() {
+
+}
