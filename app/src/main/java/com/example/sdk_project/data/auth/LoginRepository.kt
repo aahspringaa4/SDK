475303b8d11939314ce3efe1355ab9ddbf0229d5
유현명 <aahspringaa4@naver.com>
2022-01-15 17:05:52 +0900

    ⚡️:: Login Repository 구현

--- app/src/main/java/com/example/sdk_project/data/auth/LoginRepository.kt
+++ app/src/main/java/com/example/sdk_project/data/auth/LoginRepository.kt
@@ -1,4 +1,17 @@
 package com.example.sdk_project.data.auth
 
+import com.example.sdk_project.data.authApi
+import com.example.sdk_project.ui.auth.dto.LoginRequestDTO
+import com.example.sdk_project.ui.auth.dto.LoginResponseDTO
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import retrofit2.Response
+
 class LoginRepository {
+    fun login(loginRequestDTO: LoginRequestDTO) : @NonNull Single<Response<LoginResponseDTO>> =
+        authApi.postLogin(loginRequestDTO)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
 }
