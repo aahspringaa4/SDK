ce285a67cddab353571ee5b15559876cd0027920
유현명 <aahspringaa4@naver.com>
2022-01-15 15:01:48 +0900

    📑:: DataBinding & 상속 구현

--- app/src/main/java/com/example/sdk_project/ui/postdetail/PostDetailActivity.kt
+++ app/src/main/java/com/example/sdk_project/ui/postdetail/PostDetailActivity.kt
@@ -2,10 +2,19 @@ package com.example.sdk_project.ui.postdetail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sdk_project.R
+import com.example.sdk_project.base.BaseActivity
+import com.example.sdk_project.databinding.ActivityLoginBinding
+import com.example.sdk_project.databinding.ActivityPostDetailBinding
 
-class PostDetailActivity : AppCompatActivity() {
+class PostDetailActivity : BaseActivity<ActivityPostDetailBinding>(
+    R.layout.activity_post_detail
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post_detail)
+    }
+
+    override fun observeEvent() {
+        TODO("Not yet implemented")
     }
 }
