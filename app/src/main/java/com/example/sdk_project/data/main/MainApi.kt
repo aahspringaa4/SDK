92355070fba1ddd7f300c5a5d39c4890983ef936
유현명 <aahspringaa4@naver.com>
2022-01-15 16:50:38 +0900

    ♻️: 미사용 Import 삭제

--- app/src/main/java/com/example/sdk_project/data/main/MainApi.kt
+++ app/src/main/java/com/example/sdk_project/data/main/MainApi.kt
@@ -1,14 +1,11 @@
 package com.example.sdk_project.data.main
 
-import com.example.sdk_project.ui.createpost.dto.CreatePostRequestDTO
-import com.example.sdk_project.ui.createpost.dto.CreatePostResponseDTO
 import com.example.sdk_project.ui.main.dto.MainRequestDTO
 import com.example.sdk_project.ui.main.dto.MainResponseDTO
 import io.reactivex.rxjava3.core.Single
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface MainApi {
