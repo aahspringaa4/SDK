4517d85fc00230972e84d41f2ae9ec85f8562245
유현명 <aahspringaa4@naver.com>
2022-01-16 01:28:00 +0900

    ⚡️:: 카테고리로 검색 추가

--- app/src/main/java/com/example/sdk_project/data/main/MainApi.kt
+++ app/src/main/java/com/example/sdk_project/data/main/MainApi.kt
@@ -1,14 +1,23 @@
 package com.example.sdk_project.data.main
 
+import com.example.sdk_project.ui.main.dto.CategoryResponseDTO
 import com.example.sdk_project.ui.main.dto.MainResponseDTO
 import io.reactivex.rxjava3.core.Single
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface MainApi {
     @GET("post")
     fun readPostList(
-        @Header("Authorization") accessToken: String
+        @Header("Authorization") accessToken: String,
     ) : Single<Response<MainResponseDTO>>
+
+    @POST("post/category")
+    fun categoryPostList(
+        @Header("Authorization") accessToken: String,
+        @Body category: String
+    ) : Single<Response<CategoryResponseDTO>>
 }
