a83504a70f64af75f4d87239843742d415fd3988
유현명 <aahspringaa4@naver.com>
2022-01-15 18:02:34 +0900

    ⚡️:: MainRepository 구현

--- app/src/main/java/com/example/sdk_project/data/main/MainRepository.kt
+++ app/src/main/java/com/example/sdk_project/data/main/MainRepository.kt
@@ -1,4 +1,17 @@
 package com.example.sdk_project.data.main
 
+import com.example.sdk_project.data.ACCESS_TOKEN
+import com.example.sdk_project.data.mainApi
+import com.example.sdk_project.ui.main.dto.MainResponseDTO
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import retrofit2.Response
+
 class MainRepository {
+    fun readPostList() : @NonNull Single<Response<MainResponseDTO>> =
+        mainApi.readPostList(ACCESS_TOKEN)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
 }
