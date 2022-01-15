4af15b61988b1402dfa5295038ef0688514145b1
유현명 <aahspringaa4@naver.com>
2022-01-15 22:10:15 +0900

    ⚡️:: ProfileViewModel 구현

--- app/src/main/java/com/example/sdk_project/ui/profile/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/example/sdk_project/ui/profile/viewmodel/ProfileViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.sdk_project.ui.profile.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.sdk_project.data.auth.LoginRepository
 import com.example.sdk_project.data.profile.ProfileRepository
@@ -7,5 +8,17 @@ import com.example.sdk_project.data.profile.ProfileRepository
 class ProfileViewModel(
     private val rp: ProfileRepository
 ) : ViewModel() {
+    private val success : MutableLiveData<Boolean> = MutableLiveData()
+    private val failed : MutableLiveData<Boolean> = MutableLiveData()
+    lateinit var userId: String
 
+    fun readProfile() {
+        rp.readProfile(userId).subscribe { response ->
+            if(response.isSuccessful) {
+                success.value = true
+            } else {
+                failed.value = true
+            }
+        }
+    }
 }
