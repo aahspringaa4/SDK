a0d9f50c79f69a73f511cb03d8fb215a268f6b2f
유현명 <aahspringaa4@naver.com>
2022-01-15 18:03:24 +0900

    ⚡️:: PostRepository 구현

--- app/src/main/java/com/example/sdk_project/data/postdetail/PostRepository.kt
+++ app/src/main/java/com/example/sdk_project/data/postdetail/PostRepository.kt
@@ -1,4 +1,17 @@
 package com.example.sdk_project.data.postdetail
 
+import com.example.sdk_project.data.ACCESS_TOKEN
+import com.example.sdk_project.data.postApi
+import com.example.sdk_project.ui.postdetail.dto.PostResponseDTO
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import retrofit2.Response
+
 class PostRepository {
+    fun readPostDetail(id: Int) : @NonNull Single<Response<PostResponseDTO>> =
+        postApi.readPostDetail(id, ACCESS_TOKEN)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
 }
