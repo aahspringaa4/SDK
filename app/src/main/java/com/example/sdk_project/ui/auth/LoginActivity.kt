fe2fddf88d66ef81ff3c27bb7611c797e58e9380
유현명 <aahspringaa4@naver.com>
2022-01-15 14:56:28 +0900

    📑:: Login Activity & Databinding 구현 & 상속 구현

---
+++ app/src/main/java/com/example/sdk_project/ui/auth/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.example.sdk_project.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sdk_project.R
+import com.example.sdk_project.base.BaseActivity
+import com.example.sdk_project.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(
+    R.layout.activity_login
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun observeEvent() {
+        TODO("Not yet implemented")
+    }
+}
