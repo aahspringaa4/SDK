0b315f0cd6185173fdd54d9c13fc292efc610a68
유현명 <aahspringaa4@naver.com>
2022-01-15 21:59:22 +0900

    ⚡️:: mypage Api 구현

--- app/src/main/java/com/example/sdk_project/data/mypage/MyPageApi.kt
+++ app/src/main/java/com/example/sdk_project/data/mypage/MyPageApi.kt
@@ -1,5 +1,15 @@
 package com.example.sdk_project.data.mypage
 
-interface MyPageApi{
+import com.example.sdk_project.ui.mypage.dto.MypageResponseDTO
+import io.reactivex.rxjava3.core.Single
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
 
+interface MyPageApi{
+    @GET("user/profile")
+    fun readMyPage(
+        @Header("Authorization") accessToken: String
+    ): Single<Response<MypageResponseDTO>>
 }
