3b9bd102c231f8ee122caa75532525cd8b3f6da1
유현명 <aahspringaa4@naver.com>
2022-01-15 15:47:37 +0900

    ⚡️:: Profile DI Module 구현

--- app/src/main/java/com/example/sdk_project/di/module/ProfileModule.kt
+++ app/src/main/java/com/example/sdk_project/di/module/ProfileModule.kt
@@ -1,2 +1,11 @@
 package com.example.sdk_project.di.module
 
+import com.example.sdk_project.data.profile.ProfileRepository
+import com.example.sdk_project.ui.profile.viewmodel.ProfileViewModel
+import org.koin.dsl.module
+import org.koin.androidx.viewmodel.dsl.viewModel
+
+val ProfileModule = module {
+    single { ProfileRepository() }
+    viewModel { ProfileViewModel(get()) }
+}
