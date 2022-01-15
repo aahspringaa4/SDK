4d663ddbf35e84316422885b25936aa5ab00e1c4
유현명 <aahspringaa4@naver.com>
2022-01-15 17:06:07 +0900

    ⚡️:: RegisterRepository 구현

--- app/src/main/java/com/example/sdk_project/data/auth/RegisterRepository.kt
+++ app/src/main/java/com/example/sdk_project/data/auth/RegisterRepository.kt
@@ -1,4 +1,17 @@
 package com.example.sdk_project.data.auth
 
+import com.example.sdk_project.data.authApi
+import com.example.sdk_project.ui.auth.dto.RegisterRequestDTO
+import com.example.sdk_project.ui.auth.dto.RegisterResponseDTO
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import retrofit2.Response
+
 class RegisterRepository {
+    fun register(registerRequestDTO: RegisterRequestDTO) : @NonNull Single<Response<RegisterResponseDTO>> =
+        authApi.postRegister(registerRequestDTO)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
 }
