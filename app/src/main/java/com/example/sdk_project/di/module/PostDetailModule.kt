055b58b566baa0a6885b12fa56db54258c1496dd
유현명 <aahspringaa4@naver.com>
2022-01-15 15:47:23 +0900

    ⚡️:: PostDetail DI Module 구현

--- app/src/main/java/com/example/sdk_project/di/module/PostDetailModule.kt
+++ app/src/main/java/com/example/sdk_project/di/module/PostDetailModule.kt
@@ -1,2 +1,11 @@
 package com.example.sdk_project.di.module
 
+import com.example.sdk_project.data.postdetail.PostRepository
+import com.example.sdk_project.ui.postdetail.viewmodel.PostDetailViewModel
+import org.koin.dsl.module
+import org.koin.androidx.viewmodel.dsl.viewModel
+
+val PostDetailModule = module {
+    single { PostRepository() }
+    viewModel { PostDetailViewModel(get()) }
+}
