92fb2daf551ddcfc003648ec0eae7c76a1b3b4e0
유현명 <aahspringaa4@naver.com>
2022-01-15 16:50:03 +0900

    ⚡️:: Response값 구현

--- app/src/main/java/com/example/sdk_project/ui/auth/dto/LoginResponseDTO.kt
+++ app/src/main/java/com/example/sdk_project/ui/auth/dto/LoginResponseDTO.kt
@@ -1,4 +1,8 @@
 package com.example.sdk_project.ui.auth.dto
 
-class LoginResponseDTO {
-}
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponseDTO(
+    @SerializedName("access_token") val accessToken: String,
+    @SerializedName("refresh_token") val refreshToken: String
+)
