5d5053128ff1be4f28cc86669bf2c776d89c1f7b
유현명 <aahspringaa4@naver.com>
2022-01-15 17:28:23 +0900

    ⚡️:: 글쓰기 Repository 구현

--- app/src/main/java/com/example/sdk_project/data/createpost/CreatePostRepository.kt
+++ app/src/main/java/com/example/sdk_project/data/createpost/CreatePostRepository.kt
@@ -1,4 +1,18 @@
 package com.example.sdk_project.data.createpost
 
+import com.example.sdk_project.data.ACCESS_TOKEN
+import com.example.sdk_project.data.createPostApi
+import com.example.sdk_project.ui.createpost.dto.CreatePostRequestDTO
+import com.example.sdk_project.ui.createpost.dto.CreatePostResponseDTO
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import retrofit2.Response
+
 class CreatePostRepository {
+    fun createPost(createPostRequestDTO: CreatePostRequestDTO) : @NonNull Single<Response<CreatePostResponseDTO>> =
+        createPostApi.createPost(ACCESS_TOKEN, createPostRequestDTO)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
 }
