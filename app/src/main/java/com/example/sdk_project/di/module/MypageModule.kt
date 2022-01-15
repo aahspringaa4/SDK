64015e6af86db3b27aa8f95e938d33923345bdd8
유현명 <aahspringaa4@naver.com>
2022-01-15 15:47:08 +0900

    ⚡️:: Mypage DI Module 구현

--- app/src/main/java/com/example/sdk_project/di/module/MypageModule.kt
+++ app/src/main/java/com/example/sdk_project/di/module/MypageModule.kt
@@ -1,2 +1,11 @@
 package com.example.sdk_project.di.module
 
+import com.example.sdk_project.data.mypage.MypageRepository
+import com.example.sdk_project.ui.mypage.viewmodel.MypageViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val MypageModule = module {
+    single { MypageRepository() }
+    viewModel { MypageViewModel(get()) }
+}
