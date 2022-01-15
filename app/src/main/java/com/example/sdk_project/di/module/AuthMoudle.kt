07d04d52c35cff68288881bab007de16e13c9597
유현명 <aahspringaa4@naver.com>
2022-01-15 15:46:35 +0900

    ⚡️:: Auth Di Module 구현

--- app/src/main/java/com/example/sdk_project/di/module/AuthMoudle.kt
+++ app/src/main/java/com/example/sdk_project/di/module/AuthMoudle.kt
@@ -1,2 +1,16 @@
 package com.example.sdk_project.di.module
 
+import com.example.sdk_project.data.auth.LoginRepository
+import com.example.sdk_project.data.auth.RegisterRepository
+import com.example.sdk_project.ui.auth.viewmodel.LoginViewModel
+import com.example.sdk_project.ui.auth.viewmodel.RegisterViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val AuthModule = module {
+    single { LoginRepository() }
+    viewModel { LoginViewModel(get()) }
+
+    single { RegisterRepository() }
+    viewModel { RegisterViewModel(get()) }
+}
