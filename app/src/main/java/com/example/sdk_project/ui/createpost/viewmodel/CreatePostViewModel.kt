70e256bc1948e9901e55c96b7a5a0c15b56a1551
유현명 <aahspringaa4@naver.com>
2022-01-15 22:38:07 +0900

    ♻️:: 미사용 import제거

--- app/src/main/java/com/example/sdk_project/ui/createpost/viewmodel/CreatePostViewModel.kt
+++ app/src/main/java/com/example/sdk_project/ui/createpost/viewmodel/CreatePostViewModel.kt
@@ -2,11 +2,7 @@ package com.example.sdk_project.ui.createpost.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.sdk_project.data.ACCESS_TOKEN
-import com.example.sdk_project.data.REFRESH_TOKEN
 import com.example.sdk_project.data.createpost.CreatePostRepository
-import com.example.sdk_project.data.profile.ProfileRepository
-import com.example.sdk_project.ui.auth.dto.LoginRequestDTO
 import com.example.sdk_project.ui.createpost.dto.CreatePostRequestDTO
 
 class CreatePostViewModel(
