bee10059a83f93a99dc517914dd5b6adec164c4d
유현명 <aahspringaa4@naver.com>
2022-01-15 18:03:59 +0900

    ⚡️:: MainViewModel 구현

--- app/src/main/java/com/example/sdk_project/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/sdk_project/ui/main/viewmodel/MainViewModel.kt
@@ -1,11 +1,25 @@
 package com.example.sdk_project.ui.main.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.sdk_project.data.auth.RegisterRepository
 import com.example.sdk_project.data.main.MainRepository
+import com.example.sdk_project.ui.createpost.dto.CreatePostRequestDTO
+import kotlin.properties.Delegates
 
 class MainViewModel(
     private val rp: MainRepository
 ) : ViewModel() {
+    private val success : MutableLiveData<Boolean> = MutableLiveData()
+    private val failed : MutableLiveData<Boolean> = MutableLiveData()
 
+    fun readPostList() {
+        rp.readPostList().subscribe { response ->
+            if(response.isSuccessful) {
+                success.value = true
+            } else {
+                failed.value = true
+            }
+        }
+    }
 }
