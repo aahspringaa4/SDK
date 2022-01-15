8f64853b88e4642cc67e3bbff8b6c7f2dc1dfbc2
유현명 <aahspringaa4@naver.com>
2022-01-15 22:10:04 +0900

    ⚡️:: MypageViewModel 구현

--- app/src/main/java/com/example/sdk_project/ui/mypage/viewmodel/MypageViewModel.kt
+++ app/src/main/java/com/example/sdk_project/ui/mypage/viewmodel/MypageViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.sdk_project.ui.mypage.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.sdk_project.data.auth.LoginRepository
 import com.example.sdk_project.data.mypage.MypageRepository
@@ -7,5 +8,15 @@ import com.example.sdk_project.data.mypage.MypageRepository
 class MypageViewModel(
     private val rp: MypageRepository
 ) : ViewModel() {
-
+    private val success : MutableLiveData<Boolean> = MutableLiveData()
+    private val failed : MutableLiveData<Boolean> = MutableLiveData()
+    fun readMyPage() {
+        rp.readMyPage().subscribe { response ->
+            if(response.isSuccessful) {
+                success.value = true
+            } else {
+                failed.value = true
+            }
+        }
+    }
 }
