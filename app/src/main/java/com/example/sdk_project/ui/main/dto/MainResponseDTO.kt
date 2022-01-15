5ee970c02c8fe5e6e319623544f793df1e494591
유현명 <aahspringaa4@naver.com>
2022-01-15 22:09:51 +0900

    ⚡️:: Response 값 추가

--- app/src/main/java/com/example/sdk_project/ui/main/dto/MainResponseDTO.kt
+++ app/src/main/java/com/example/sdk_project/ui/main/dto/MainResponseDTO.kt
@@ -3,12 +3,14 @@ package com.example.sdk_project.ui.main.dto
 import com.google.gson.annotations.SerializedName
 
 data class MainResponseDTO(
-    @SerializedName("post") val post: PostList
+    @SerializedName("post") val post: List<PostList>
 ){
     data class PostList(
+        @SerializedName("id") val postId: Int,
         @SerializedName("title") val postTitle: String,
         @SerializedName("url") val postUrl: String,
         @SerializedName("category") val postCategory: String,
-        @SerializedName("user") val writer:String
+        @SerializedName("user") val writer: String,
+        @SerializedName("createAt") val createAt: String
     )
 }
