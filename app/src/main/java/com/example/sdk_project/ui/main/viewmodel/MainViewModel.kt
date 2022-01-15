39890976525336dec88c3a314d13514dfb57827c
유현명 <aahspringaa4@naver.com>
2022-01-15 15:49:25 +0900

    ⚡️:: MainViewModel 상속 구현

---
+++ app/src/main/java/com/example/sdk_project/ui/main/viewmodel/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.sdk_project.ui.main.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.sdk_project.data.auth.RegisterRepository
+import com.example.sdk_project.data.main.MainRepository
+
+class MainViewModel(
+    private val rp: MainRepository
+) : ViewModel() {
+
+}
