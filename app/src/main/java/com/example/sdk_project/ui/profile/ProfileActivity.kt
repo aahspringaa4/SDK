920cfe879f6e2cb057ac62040ffb198a2b17989d
유현명 <aahspringaa4@naver.com>
2022-01-15 15:01:52 +0900

    📑:: DataBinding & 상속 구현

--- app/src/main/java/com/example/sdk_project/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/example/sdk_project/ui/profile/ProfileActivity.kt
@@ -2,10 +2,19 @@ package com.example.sdk_project.ui.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sdk_project.R
+import com.example.sdk_project.base.BaseActivity
+import com.example.sdk_project.databinding.ActivityLoginBinding
+import com.example.sdk_project.databinding.ActivityProfileBinding
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : BaseActivity<ActivityProfileBinding>(
+    R.layout.activity_profile
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+    }
+
+    override fun observeEvent() {
+        TODO("Not yet implemented")
     }
 }
