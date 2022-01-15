f3c2a16b6e1eabb82f9b00a422f134e7fc8ad62a
유현명 <aahspringaa4@naver.com>
2022-01-15 15:22:01 +0900

    ⚡️:: DI Application

--- app/src/main/java/com/example/sdk_project/di/SdkApplication.kt
+++ app/src/main/java/com/example/sdk_project/di/SdkApplication.kt
@@ -1,4 +1,25 @@
 package com.example.sdk_project.di
 
-class SdkApplication {
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class SdkApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@SdkApplication)
+
+            modules(
+                listOf(
+                    loginModule,
+                    registerModule,
+                    mainModule,
+                    myPageModule,
+                    postModule
+                )
+            )
+        }
+    }
 }
