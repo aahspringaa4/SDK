6f384fed7a489286c151d9f9528b9b98c708e822
유현명 <aahspringaa4@naver.com>
2022-01-15 15:46:56 +0900

    ⚡️:: Main DI Module 구현

--- app/src/main/java/com/example/sdk_project/di/module/MainModule.kt
+++ app/src/main/java/com/example/sdk_project/di/module/MainModule.kt
@@ -1,2 +1,11 @@
 package com.example.sdk_project.di.module
 
+import com.example.sdk_project.data.main.MainRepository
+import com.example.sdk_project.ui.main.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val MainModule = module {
+    single { MainRepository() }
+    viewModel { MainViewModel(get()) }
+}
