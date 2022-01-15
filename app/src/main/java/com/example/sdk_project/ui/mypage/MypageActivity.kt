5ed5630b174eb56d4edfa99415e15cd9348108db
유현명 <aahspringaa4@naver.com>
2022-01-15 15:01:43 +0900

    📑:: DataBinding & 상속 구현

--- app/src/main/java/com/example/sdk_project/ui/mypage/MypageActivity.kt
+++ app/src/main/java/com/example/sdk_project/ui/mypage/MypageActivity.kt
@@ -2,10 +2,19 @@ package com.example.sdk_project.ui.mypage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sdk_project.R
+import com.example.sdk_project.base.BaseActivity
+import com.example.sdk_project.databinding.ActivityLoginBinding
+import com.example.sdk_project.databinding.ActivityMypageBinding
 
-class MypageActivity : AppCompatActivity() {
+class MypageActivity : BaseActivity<ActivityMypageBinding>(
+    R.layout.activity_mypage
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mypage)
+    }
+
+    override fun observeEvent() {
+        TODO("Not yet implemented")
     }
 }
