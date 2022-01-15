3556c08a334fc00726bbfff7148f4cfe2f505771
유현명 <aahspringaa4@naver.com>
2022-01-15 16:22:08 +0900

    📑:: 게시물 만들기 ViewModel 구현

---
+++ app/src/main/java/com/example/sdk_project/ui/createpost/viewmodel/CreatePostViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.sdk_project.ui.createpost.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.sdk_project.data.createpost.CreatePostRepository
+import com.example.sdk_project.data.profile.ProfileRepository
+
+class CreatePostViewModel(
+    private val rp: CreatePostRepository
+) : ViewModel() {
+
+}
