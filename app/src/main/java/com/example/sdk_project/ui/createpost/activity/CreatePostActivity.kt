8483d39fbb95171c93424959c844b38d2a0a8582
유현명 <aahspringaa4@naver.com>
2022-01-15 16:21:54 +0900

    📑:: 게시물 만들기 Activity 구현

---
+++ app/src/main/java/com/example/sdk_project/ui/createpost/activity/CreatePostActivity.kt
@@ -0,0 +1,20 @@
+package com.example.sdk_project.ui.createpost.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sdk_project.R
+import com.example.sdk_project.base.BaseActivity
+import com.example.sdk_project.databinding.ActivityCreatePostBinding
+import com.example.sdk_project.databinding.ActivityLoginBinding
+
+class CreatePostActivity : BaseActivity<ActivityCreatePostBinding>(
+    R.layout.activity_create_post
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun observeEvent() {
+        TODO("Not yet implemented")
+    }
+}
