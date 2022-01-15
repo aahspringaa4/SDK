bfca96f6d04c711f2c035de875796831120d6bb2
유현명 <aahspringaa4@naver.com>
2022-01-15 17:05:34 +0900

    ⚡️:: Response 클래스 추가

--- app/src/main/java/com/example/sdk_project/data/auth/AuthApi.kt
+++ app/src/main/java/com/example/sdk_project/data/auth/AuthApi.kt
@@ -3,6 +3,7 @@ package com.example.sdk_project.data.auth
 import com.example.sdk_project.ui.auth.dto.LoginRequestDTO
 import com.example.sdk_project.ui.auth.dto.LoginResponseDTO
 import com.example.sdk_project.ui.auth.dto.RegisterRequestDTO
+import com.example.sdk_project.ui.auth.dto.RegisterResponseDTO
 import io.reactivex.rxjava3.core.Single
 import retrofit2.Response
 import retrofit2.http.Body
@@ -10,7 +11,9 @@ import retrofit2.http.POST
 
 interface AuthApi {
     @POST("user/signup")
-    fun postRegister(@Body registerRequestDTO: RegisterRequestDTO)
+    fun postRegister(
+        @Body registerRequestDTO: RegisterRequestDTO
+    ): Single<Response<RegisterResponseDTO>>
 
     @POST("user/signin")
     fun postLogin(
